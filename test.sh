3f589d51f1019f5a4ec2b8fc7c1a5b22ac1c9052
MrY <yshi.riaru@gmail.com>
2024-04-10 15:33:18 +0000

    テストを書き換え

--- test.sh
+++ test.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 assert() {
-  expected="$1"
-  input="$2"
 
-  ./9cc "$input" > tmp.s
-  cc -o tmp tmp.s
-  ./tmp
-  actual="$?"
+    expected="$1"
+    input="$2"
 
-  if [ "$actual" = "$expected" ]; then
-    echo "$input => $actual"
-  else
-    echo "$input => $expected expected, but got $actual"
-    exit 1
-  fi
+    ./9cc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
 }
 
-assert 0 0
-assert 42 42
+assert 1+1 2
+assert 4+2 6
 
 echo OK
