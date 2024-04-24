31dd5d6bac36014e72a58b6cce0ab44d6fd8d45d
MrY <yshi.riaru@gmail.com>
2024-04-25 00:50:54 +0900

    mulのテスト追加

--- test.sh
+++ test.sh
@@ -18,10 +18,11 @@ assert() {
 }
 
 echo テスト開始
-#assert 6 "3*2"
-#assert 7 " 4 + (2 - 1)*3"
-#assert 3 "6/3+1"
-#assert 1 "-1+2"
+assert 6 "3*2"
+assert 6 "1*2*3"
+assert 7 " 4 + (2 - 1)*3"
+assert 3 "6/3+1"
+assert 1 "-1+2"
 
 assert 1 "2==2"
 assert 0 "2==3"
