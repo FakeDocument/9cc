6d35f473d4c95b0c7efc79a43059a6d64f80cabd
MrY <yshi.riaru@gmail.com>
2024-07-04 00:32:49 +0900

    テスト変更

--- test.sh
+++ test.sh
@@ -18,32 +18,6 @@ assert() {
 }
 
 echo テスト開始
-assert 6 "3*2;"
-assert 6 "1*2*3;"
-assert 7 " 4 + (2 - 1)*3;"
-assert 3 "6/3+1;"
-assert 1 "-1+2;"
-
-assert 1 "2==2;"
-assert 0 "2==3;"
-assert 1 "2!=3;"
-assert 0 "2!=2;"
-
-assert 1 "1<2;"
-assert 0 "3<2;"
-
-assert 0 "1>2;"
-assert 1 "3>2;"
-
-assert 1 "1<=1;"
-assert 1 "1<=2;"
-assert 0 "3<=2;"
-
-assert 1 "1>=1;"
-assert 1 "2>=1;"
-assert 0 "2>=3;"
-
-assert 2 "a=1;b=2;a*b;"
-assert 2 "hoge=1;b=2;hoge*b;"
+assert 2 "a=1;b=2;hoge=a*b;return hoge;"
 
 echo OK
