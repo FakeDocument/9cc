0d579e9b33b908831b240769a8426cc6e1e38d87
MrY <yshi.riaru@gmail.com>
2024-07-17 18:27:05 +0900

    while文のテストを追加

--- test.sh
+++ test.sh
@@ -21,4 +21,5 @@ echo テスト開始
 assert 2 "a=1;b=2;hoge=a*b;return hoge;"
 assert 4 "a=2;if(a==2)a=4;return a;"
 assert 0 "a=1;if(a==2)a=4;else a=0; return a;"
+assert 5 "a=0;while(a<5)a=a+1;return a;"
 echo OK
